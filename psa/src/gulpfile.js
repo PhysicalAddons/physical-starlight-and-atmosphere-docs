a60d47e1a7d1324371b586fd618921ee977add07
karlisup <karlis@upitis.com>
2021-10-29 14:41:13 +0300

    Wonderfully overengineered image setup

--- psa/src/gulpfile.js
+++ psa/src/gulpfile.js
@@ -74,151 +74,169 @@ gulp.task("images-presets", function () {
 });
 
 gulp.task("images-presets", function () {
-	return gulp
-	  .src("images/presets/*.jpg")
-	  .pipe(
-		responsive(
-		  {
-			"*.jpg": [
-			  {
-				width: 300,
-				rename: { suffix: "-300" },
-			  },
-			  {
-				width: 600,
-				rename: { suffix: "-600" },
-			  },
-			  {
-				width: 1200,
-				rename: { suffix: "-1200" },
-			  },
-			  {
-				width: 1600,
-				rename: { suffix: "-1600" },
-			  },
-			],
-		  },
-		  {
-			// Global configuration for all images
-			quality: 95,
-			progressive: true,
-			withMetadata: false,
-		  }
-		)
-	  )
-	  .pipe(gulp.dest(destroot + "assets/images/presets"));
-  });
+  return gulp
+    .src("images/presets/*.jpg")
+    .pipe(
+      responsive(
+        {
+          "*.jpg": [
+            {
+              width: 300,
+              rename: { suffix: "-300" },
+            },
+            {
+              width: 600,
+              rename: { suffix: "-600" },
+            },
+            {
+              width: 1200,
+              rename: { suffix: "-1200" },
+            },
+            {
+              width: 1600,
+              rename: { suffix: "-1600" },
+            },
+          ],
+        },
+        {
+          // Global configuration for all images
+          quality: 95,
+          progressive: true,
+          withMetadata: false,
+        }
+      )
+    )
+    .pipe(gulp.dest(destroot + "assets/images/presets"));
+});
 
-  gulp.task("images-gallery", function () {
-	return gulp
-	  .src("images/gallery/**/*thumb*.jpg")
-	  .pipe(
-		responsive(
-		  {
-			"**/*-thumb.jpg": [
-			  {
-				width: 147,
-				rename: { suffix: "-300" },
-			  },
-			  {
-				width: 285,
-				rename: { suffix: "-575" },
-			  },
-			  {
-				width: 424,
-				rename: { suffix: "-1704" },
-			  },
-			],
-			"**/*-thumb-2x.jpg": [
-				{
-				  width: 296,
-				  rename: { suffix: "-300" },
-				},
-				{
-				  width: 571,
-				  rename: { suffix: "-575" },
-				},
-				{
-				  width: 849,
-				  rename: { suffix: "-1704" },
-				},
-			  ],
-		  },
-		  {
-			// Global configuration for all images
-			quality: 95,
-			progressive: true,
-			withMetadata: false,
-		  }
-		)
-	  )
-	  .pipe(gulp.dest(destroot + "assets/images/gallery"));
-  });
+gulp.task("images-gallery", function () {
+  return gulp
+    .src("images/gallery/**/*.jpg")
+    .pipe(
+      responsive(
+        {
+          "**/*{1,2,3,4,5}.jpg": [
+            {},
+          ],
+          "**/*-thumb.jpg": [
+            {
+              width: 160,
+              rename: { suffix: "-160" },
+            },
+            {
+              width: 200,
+              rename: { suffix: "-200" },
+            },
+            {
+              width: 256,
+              rename: { suffix: "-256" },
+            },
+            {
+              width: 360,
+              rename: { suffix: "-360" },
+            },
+            {
+              width: 425,
+              rename: { suffix: "-425" },
+            },
+          ],
+          "**/*-thumb-2x.jpg": [
+            {
+              width: 320,
+              rename: { suffix: "-320" },
+            },
+            {
+              width: 400,
+              rename: { suffix: "-400" },
+            },
+            {
+              width: 512,
+              rename: { suffix: "-512" },
+            },
+            {
+              width: 720,
+              rename: { suffix: "-720" },
+            },
+            {
+              width: 850,
+              rename: { suffix: "-850" },
+            },
+          ],
+        },
+        {
+          // Global configuration for all images
+          quality: 95,
+          progressive: true,
+          withMetadata: false,
+        }
+      )
+    )
+    .pipe(gulp.dest(destroot + "assets/images/gallery"));
+});
 
-  gulp.task("images-testimonials", function () {
-	return gulp
-	  .src("images/testimonials/*.jpg")
-	  .pipe(
-		responsive(
-		  {
-			"*.jpg": [
-			  {
-				width: 100,
-				rename: { suffix: "-100" },
-			  },
-			  {
-				width: 200,
-				rename: { suffix: "-200" },
-			  },
-			],
-		  },
-		  {
-			// Global configuration for all images
-			quality: 95,
-			progressive: true,
-			withMetadata: false,
-		  }
-		)
-	  )
-	  .pipe(gulp.dest(destroot + "assets/images/testimonials"));
-  });
+gulp.task("images-testimonials", function () {
+  return gulp
+    .src("images/testimonials/*.jpg")
+    .pipe(
+      responsive(
+        {
+          "*.jpg": [
+            {
+              width: 100,
+              rename: { suffix: "-100" },
+            },
+            {
+              width: 200,
+              rename: { suffix: "-200" },
+            },
+          ],
+        },
+        {
+          // Global configuration for all images
+          quality: 95,
+          progressive: true,
+          withMetadata: false,
+        }
+      )
+    )
+    .pipe(gulp.dest(destroot + "assets/images/testimonials"));
+});
 
-  gulp.task("images-ourstory", function () {
-	return gulp
-	  .src("images/our-story/*.jpg")
-	  .pipe(
-		responsive(
-		  {
-			"*.jpg": [
-			  {
-				width: 100,
-				rename: { suffix: "-200" },
-			  },
-			  {
-				width: 320,
-				rename: { suffix: "-320" },
-			  },
-			  {
-				width: 450,
-				rename: { suffix: "-450" },
-			  },
-			  {
-				width: 600,
-				rename: { suffix: "-600" },
-			  },
-			],
-		  },
-		  {
-			// Global configuration for all images
-			quality: 95,
-			progressive: true,
-			withMetadata: false,
-		  }
-		)
-	  )
-	  .pipe(gulp.dest(destroot + "assets/images/our-story"));
-  });
-
+gulp.task("images-ourstory", function () {
+  return gulp
+    .src("images/our-story/*.jpg")
+    .pipe(
+      responsive(
+        {
+          "*.jpg": [
+            {
+              width: 200,
+              rename: { suffix: "-200" },
+            },
+            {
+              width: 320,
+              rename: { suffix: "-320" },
+            },
+            {
+              width: 450,
+              rename: { suffix: "-450" },
+            },
+            {
+              width: 600,
+              rename: { suffix: "-600" },
+            },
+          ],
+        },
+        {
+          // Global configuration for all images
+          quality: 95,
+          progressive: true,
+          withMetadata: false,
+        }
+      )
+    )
+    .pipe(gulp.dest(destroot + "assets/images/our-story"));
+});
 
 gulp.task("watch", function () {
   gulp.watch("./**/*.scss", gulp.series("css"));
